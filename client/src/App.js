e700399c13b90325359c7d35e454ee032ac3dd68
evan <aspaasevan@gmail.com>
2020-04-22 11:13:25 -0700

    added route in App for MovieForm

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Route } from "react-router-dom";
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
+import MovieForm from "./Movies/MovieForm";
 import Movie from "./Movies/Movie";
 import axios from 'axios';
 
@@ -34,6 +35,11 @@ const App = () => {
 
       <Route path="/movies/:id">
         <Movie addToSavedList={addToSavedList} />
+      </Route>
+
+      {/* Add a route at the path `/update-movie/:id` */}
+      <Route path="/update-movie/:id">
+        <MovieForm />
       </Route>
     </>
   );
---
+++ client/src/Movies/MovieForm.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import Movie from './Movie';
+
+// {
+//   id: 0,
+//   title: "The Godfather",
+//   director: "Francis Ford Coppola",
+//   metascore: 100,
+//   stars: ["Marlon Brando", "Al Pacino", "Robert Duvall"]
+// },
+
+const MovieForm = () => {
+  return (
+    <form>
+      <label>
+        Name:
+        <input></input>
+      </label>
+      <label>
+        Name:
+        <input></input>
+      </label>
+    </form>
+  )
+}
+
+export default MovieForm;
